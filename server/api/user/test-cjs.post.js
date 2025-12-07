319ce8e6b8a3adb15cc9afebf01e3ce92d6b763f
edwin fan <edwin.fan.0128@gmail.com>
2025-12-07 20:51:48 +0800

    fix bug-29

--- server/api/user/test-cjs.post.js
+++ server/api/user/test-cjs.post.js
@@ -1,16 +1,26 @@
 import { createRequire } from "node:module";
+import { join } from "node:path";
 
-const require = createRequire(import.meta.url);
+const requireCjs = createRequire(import.meta.url);
 
 export default defineEventHandler(async (event) => {
-  const { queryTx } = require("../../solana/queryTx.cjs");
-
   const body = await readBody(event);
   const { signature, mint } = body || {};
 
   if (!signature || !mint) {
     return { status: "error", message: "missing params" };
   }
+
+  // ✅ 永远指向 .output/server/solana
+  const queryTxPath = join(
+    process.cwd(),
+    ".output",
+    "server",
+    "solana",
+    "queryTx.cjs"
+  );
+
+  const { queryTx } = requireCjs(queryTxPath);
 
   const result = await queryTx(signature, mint);
 
