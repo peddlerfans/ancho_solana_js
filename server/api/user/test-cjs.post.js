342c2de8ce9bf4dc476ffbe7e9e5f62672e1fc28
edwin fan <edwin.fan.0128@gmail.com>
2025-12-07 20:12:59 +0800

    fix bug-28

--- server/api/user/test-cjs.post.js
+++ server/api/user/test-cjs.post.js
@@ -1,3 +1,7 @@
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+
 export default defineEventHandler(async (event) => {
   const { queryTx } = require("../../solana/queryTx.cjs");
 
